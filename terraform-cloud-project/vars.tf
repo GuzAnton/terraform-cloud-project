d8133c539a7004d07c0fda2b4c555eead12d258f
Tony <guzantony@gmail.com>
2023-12-17 12:00:02 +0200

    added variables

--- terraform-cloud-project/vars.tf
+++ terraform-cloud-project/vars.tf
@@ -1,3 +1,26 @@
 variable "REGION" {
   default = "eu-central-1"
 }
+
+variable "AMI" {
+  type = map(any)
+  default = {
+    eu-central-1 = "ami-06dd92ecc74fdfb36"
+    eu-west-1 = "ami-0694d931cee176e7d"
+  }
+}
+variable "PUB_KEY_PATH" {
+  default = "test.pub"
+}
+
+variable "PRIV_KEY_PATH" {
+  default = "test"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "MyIp" {
+  default = "176.36.180.171/32"
+}
